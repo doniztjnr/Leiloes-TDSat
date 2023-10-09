dfb7c9018f337557eaa48f503d30551819819b6a
Donizete P D S Junior <doniztjnr@gmail.com>
2023-10-09 17:24:17 -0300

    Implementar funcionalidade salvar produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,45 @@
  *
  * @author Adm
  */
-
+import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) throws SQLException {
+
+        conn = new conectaDAO().connectDB();
+
+        String SQL_INSERT = "INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)";
+
+        try {
+            prep = conn.prepareStatement(SQL_INSERT);
+            prep.setString(1, produto.getNome());
+            prep.setBigDecimal(2, new BigDecimal(produto.getValor()));
+            prep.setString(3, produto.getStatus());
+
+            prep.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -1,8 +1,14 @@
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
 /**
  *
  * @author Adm
@@ -135,26 +141,41 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void cadastroNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastroNomeActionPerformed
-        
-        
+
+
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
-        ProdutosDTO produto = new ProdutosDTO();
-        String nome = cadastroNome.getText();
-        String valor = cadastroValor.getText();
-        String status = "A Venda";
-        produto.setNome(nome);
-        produto.setValor(Integer.parseInt(valor));
-        produto.setStatus(status);
-        
-        ProdutosDAO produtodao = new ProdutosDAO();
-        produtodao.cadastrarProduto(produto);
-        
+        try {
+            if (!cadastroNome.getText().trim().isEmpty()
+                    && !cadastroValor.getText().trim().isEmpty()
+                    && isNumeric(cadastroValor.getText().trim())) {
+                ProdutosDTO produto = new ProdutosDTO();
+                String nome = cadastroNome.getText();
+                String valor = cadastroValor.getText();
+                String status = "A Venda";
+                produto.setNome(nome);
+                produto.setValor(Float.parseFloat(valor));
+                produto.setStatus(status);
+
+                ProdutosDAO produtodao = new ProdutosDAO();
+                produtodao.cadastrarProduto(produto);
+
+                JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
+
+                cadastroNome.setText("");
+                cadastroValor.setText("");
+                return;
+            }
+            JOptionPane.showMessageDialog(null, "Cadastro não realizado!");
+        } catch (SQLException ex) {
+            Logger.getLogger(cadastroVIEW.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void btnProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProdutosActionPerformed
-        listagemVIEW listagem = new listagemVIEW(); 
+        listagemVIEW listagem = new listagemVIEW();
         listagem.setVisible(true);
     }//GEN-LAST:event_btnProdutosActionPerformed
 
@@ -205,4 +226,16 @@ public class cadastroVIEW extends javax.swing.JFrame {
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
     // End of variables declaration//GEN-END:variables
+
+    private boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            float f = Float.parseFloat(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
 }
